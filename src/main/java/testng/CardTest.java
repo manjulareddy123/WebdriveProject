6665bafe9ef28cd19d8cfbb9a46b66d15b1dd429
manjulareddy123 <143509603+manjulareddy123@users.noreply.github.com>
2023-12-15 19:55:32 +0530

    first webdrive

---
+++ src/main/java/testng/CardTest.java
@@ -0,0 +1,11 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class CardTest extends BaseClass {
+
+@Test (groups= {"sanity"})
+public void TC01_CrediteCardTest() {
+	}
+
+}
---
+++ src/main/java/testng/LoanTest.java
@@ -0,0 +1,29 @@
+package testng;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoanTest extends BaseClass{
+	
+	@Test
+	public void TC01_HomeLoanTest() {
+		
+		System.out.println("Inside HomeLoan");
+	}
+	
+	@Test(enabled=true)
+	public void TC02_CarLoanTest() {
+		
+		System.out.println("Inside CarLoan");
+	}
+	
+	@Test (groups= {"sanity"})
+	public void TC00_PersonalLoanTest() {
+		
+		System.out.println("Inside PersonalLoan");
+	}
+
+}
